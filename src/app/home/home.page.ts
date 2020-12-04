6a7256e7a7d72be41c0aa8184132e9b45e798723
Jeffersontads <jeffersontads@hotmail.com>
2020-12-04 11:17:09 -0300

    Criar tela inicial, estilos, logotipo e icons footer

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,19 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  public options: Array<any> = [
+    { icon: 'person-add-outline', text: 'Indicar amigos'},
+    { icon: 'phone-portrait', text: 'Recarga de celular'},
+    { icon: 'wallet-outline', text: 'Depositar'},
+    { icon: 'options-outline', text: 'Ajustar limite'},
+    { icon: 'help-circle', text: 'Me ajuda'},
+    { icon: 'barcode-outline', text: 'Pagar'},
+  { icon: 'lock-open-outline', text: 'Bloquear cartão'},
+  { icon: 'card-outline', text: 'Cartão virtual'},
+  ];
+
+  public slidesOptions: any = {slidesPerView: 3, freemode: true };
+
   constructor() {}
 
 }
